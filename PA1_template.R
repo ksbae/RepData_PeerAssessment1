6e5d0527aeeca1b643d34b23d7f885dea235a4eb
Kyun-Seop Bae <ksbae@acr.kr>
2014-12-14 01:22:11 +0900

    finished

--- PA1_template.R
+++ PA1_template.R
@@ -9,55 +9,55 @@ output:
 ## Loading and preprocessing the data
 
 ### Load the data
-```{r echo=TRUE}
+#```{r echo=TRUE}
+setwd("E:/Rt/Peer1/")
 AMD = read.csv("Activity.csv", stringsAsFactors=FALSE)
-```
+#```
 
-### Preprodess/transform the data & check the integrity
-```{r echo=TRUE}
-uDate = unique(AMD[,"date"]) # what unique days 
-nDate = length(uDate) # 61 days
-uInterval = unique(AMD[,"interval"]) # what unique intervals
-nInterval = length(uInterval)  # 288 intervals
-if (nInterval != 60*24/5) print("Some intervals are missing")
+### Preprodess/transform the data
+#```{r echo=TRUE}
+uDate = unique(AMD[,"date"])
+uInterval = unique(AMD[,"interval"])  # 288
+nDate = length(uDate) # 61
 
 Lens = vector(length=nDate)
+nInterval = 60*24/5 # 60 minutes per hour * 24 hours / 5 minutes interval
 for (i in 1:nDate) Lens[i] = length(AMD[AMD[,"date"]==uDate[i],"steps"])
 if (sum(Lens != nInterval) != 0) print("Uneven measurements per day")
-```
+#```
 
 ## What is mean total number of steps taken per day?
 ### Histogram of Steps without Imputation
-```{r echo=TRUE}
+#```{r echo=TRUE}
 TotSteps = aggregate(steps ~ date, data=AMD, sum, na.rm=TRUE)
 hist(TotSteps$steps, main="Histogram of Steps without Imputation")
-```
+#```
 ### Mean and Median without Imputation
-```{r echo=TRUE}
+#```{r echo=TRUE}
 mean(TotSteps$steps)  # 10766.19
 median(TotSteps$steps) # 10765
-```
+#```
 
 ## What is the average daily activity pattern?
-```{r echo=TRUE}
+#```{r echo=TRUE}
 AvgSteps = aggregate(steps ~ interval, data=AMD, mean, na.rm=TRUE)
 plot(uInterval, AvgSteps$steps, type="l", ylab="Average Steps")
-```
+#```
 
 ### Which 5 min interval contains the maximum number?
-```{r echo=TRUE}
+#```{r echo=TRUE}
 which.max(AvgSteps$steps)  # 104th inteval
 AMD[which.max(AvgSteps$steps),"interval"] # [835, 840) interval
-```
+#```
 
 ## Imputing missing values
 ### total number of missing values
-```{r echo=TRUE}
+#```{r echo=TRUE}
 sum(is.na(AMD[,"steps"])) # 2304
-```
+#```
 
 ### Impute NA with average steps in that interval (AvgSteps)
-```{r echo=TRUE}
+#```{r echo=TRUE}
 mAMD = matrix(AMD[,"steps"], nrow=nInterval, ncol=nDate)
 for (i in 1:nInterval) {
   for (j in 1:nDate) {
@@ -67,30 +67,30 @@ for (i in 1:nInterval) {
 
 AMD2 = AMD
 AMD2[,"steps.im"] = as.vector(mAMD)
-```
+#```
 
 ### Histogram of Steps with Imputation
-```{r echo=TRUE}
+#```{r echo=TRUE}
 TotSteps2 = aggregate(steps.im ~ date, data=AMD2, sum, na.rm=TRUE)
 hist(TotSteps2$steps, main="Histogram of Steps with Imputation")
-```
+#```
 
 ### mean and median of total steps in days
-```{r echo=TRUE}
+#```{r echo=TRUE}
 mean(TotSteps2$steps) # 10766.19
 median(TotSteps2$steps) # 10766.19
-```
+#```
 
 
 ## Are there differences in activity patterns between weekdays and weekends?
-```{r echo=TRUE}
+#```{r echo=TRUE}
 weekday.no = (julian(as.Date(AMD2[,"date"])) - 3) %% 7  #0:6 is Sun:Sat
-AMD2[,"wday"] = "weekday"  # fill up with default values
+AMD2[,"wday"] = "weekday"
 AMD2[weekday.no==0 | weekday.no==6, "wday"] = "weekend"
-AMD2[,"wday"] = factor(AMD2[,"wday"]) # transform to factor
+AMD2[,"wday"] = factor(AMD2[,"wday"])
 
-Steps2 = aggregate(steps ~ interval + wday, AMD2, mean) #no need of na.rm option
+Steps2 = aggregate(steps ~ interval + wday, AMD2, mean)
 library(lattice)
 xyplot(steps ~ interval | factor(wday), data=Steps2, aspect=1/2, type="l")
-```
+#```
 
--- PA1_template.md
+++ PA1_template.md
@@ -9,12 +9,14 @@ output:
 ## Loading and preprocessing the data
 
 ### Load the data
-```{r echo=TRUE}
+
+```r
 AMD = read.csv("Activity.csv", stringsAsFactors=FALSE)
 ```
 
 ### Preprodess/transform the data & check the integrity
-```{r echo=TRUE}
+
+```r
 uDate = unique(AMD[,"date"]) # what unique days 
 nDate = length(uDate) # 61 days
 uInterval = unique(AMD[,"interval"]) # what unique intervals
@@ -28,36 +30,72 @@ if (sum(Lens != nInterval) != 0) print("Uneven measurements per day")
 
 ## What is mean total number of steps taken per day?
 ### Histogram of Steps without Imputation
-```{r echo=TRUE}
+
+```r
 TotSteps = aggregate(steps ~ date, data=AMD, sum, na.rm=TRUE)
 hist(TotSteps$steps, main="Histogram of Steps without Imputation")
 ```
+
+![plot of chunk unnamed-chunk-3](figure/unnamed-chunk-3-1.png) 
 ### Mean and Median without Imputation
-```{r echo=TRUE}
+
+```r
 mean(TotSteps$steps)  # 10766.19
+```
+
+```
+## [1] 10766.19
+```
+
+```r
 median(TotSteps$steps) # 10765
 ```
 
+```
+## [1] 10765
+```
+
 ## What is the average daily activity pattern?
-```{r echo=TRUE}
+
+```r
 AvgSteps = aggregate(steps ~ interval, data=AMD, mean, na.rm=TRUE)
 plot(uInterval, AvgSteps$steps, type="l", ylab="Average Steps")
 ```
 
+![plot of chunk unnamed-chunk-5](figure/unnamed-chunk-5-1.png) 
+
 ### Which 5 min interval contains the maximum number?
-```{r echo=TRUE}
+
+```r
 which.max(AvgSteps$steps)  # 104th inteval
+```
+
+```
+## [1] 104
+```
+
+```r
 AMD[which.max(AvgSteps$steps),"interval"] # [835, 840) interval
+```
+
+```
+## [1] 835
 ```
 
 ## Imputing missing values
 ### total number of missing values
-```{r echo=TRUE}
+
+```r
 sum(is.na(AMD[,"steps"])) # 2304
 ```
 
+```
+## [1] 2304
+```
+
 ### Impute NA with average steps in that interval (AvgSteps)
-```{r echo=TRUE}
+
+```r
 mAMD = matrix(AMD[,"steps"], nrow=nInterval, ncol=nDate)
 for (i in 1:nInterval) {
   for (j in 1:nDate) {
@@ -70,20 +108,36 @@ AMD2[,"steps.im"] = as.vector(mAMD)
 ```
 
 ### Histogram of Steps with Imputation
-```{r echo=TRUE}
+
+```r
 TotSteps2 = aggregate(steps.im ~ date, data=AMD2, sum, na.rm=TRUE)
 hist(TotSteps2$steps, main="Histogram of Steps with Imputation")
 ```
 
+![plot of chunk unnamed-chunk-9](figure/unnamed-chunk-9-1.png) 
+
 ### mean and median of total steps in days
-```{r echo=TRUE}
+
+```r
 mean(TotSteps2$steps) # 10766.19
+```
+
+```
+## [1] 10766.19
+```
+
+```r
 median(TotSteps2$steps) # 10766.19
+```
+
+```
+## [1] 10766.19
 ```
 
 
 ## Are there differences in activity patterns between weekdays and weekends?
-```{r echo=TRUE}
+
+```r
 weekday.no = (julian(as.Date(AMD2[,"date"])) - 3) %% 7  #0:6 is Sun:Sat
 AMD2[,"wday"] = "weekday"  # fill up with default values
 AMD2[weekday.no==0 | weekday.no==6, "wday"] = "weekend"
@@ -93,4 +147,6 @@ Steps2 = aggregate(steps ~ interval + wday, AMD2, mean) #no need of na.rm option
 library(lattice)
 xyplot(steps ~ interval | factor(wday), data=Steps2, aspect=1/2, type="l")
 ```
+
+![plot of chunk unnamed-chunk-11](figure/unnamed-chunk-11-1.png) 
 
